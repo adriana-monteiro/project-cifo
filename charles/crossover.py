ad7b1b7f152636690997c51e1b7852518be3fd01
goncaloaires <119682443+goncaloaires@users.noreply.github.com>
2023-05-24 23:35:23 +0100

    k point crossover implemeted

--- charles/crossover.py
+++ charles/crossover.py
@@ -1,5 +1,7 @@
-from random import randint, sample, uniform
+from random import randint, sample, uniform,seed
 
+#se = 69
+#seed(se)
 
 def single_point_co(p1, p2):
     """Implementation of single point crossover.
@@ -55,11 +57,9 @@ def cycle_xo(p1, p2):
 
     return offspring1, offspring2
 
-def pmx(p1,p2):
-    # xo_points =  sample(range(len(p1)), 2)
-    # xo_points.sort() 
-
-    xo_points= [3,6]
+def pmx_co(p1,p2):
+    xo_points =  sample(range(len(p1)), 2)
+    xo_points.sort() 
 
     def pmx_offspring(x,y):
         o = [None] * len(x)
@@ -67,7 +67,6 @@ def pmx(p1,p2):
         # offspring 2
         o[xo_points[0]:xo_points[1]] = x[xo_points[0]:xo_points[1]]
         z = set(y[xo_points[0]:xo_points[1]]) - set(x[xo_points[0]:xo_points[1]])
-
         for i in z:
             temp = i
             index = y.index(x[y.index(temp)])
@@ -103,7 +102,49 @@ def arithmetic_xo(p1,p2): #cant use it if we want only integers in the solutions
     return o1,o2
 
 
+def k_point_co(p1,p2,k):
+    offspring1 = [None] * len(p1)
+    offspring2 = [None] * len(p1)
+    xo_points = sample(range(1,len(p1)), k)
+    xo_points.sort()
+
+    for i in range(xo_points[0]):
+        offspring1[i]=p1[i]
+        offspring2[i]=p2[i]
+    switch=0
+    for j in range(1,k):
+        if switch==0:
+            for i in range(xo_points[j-1],xo_points[j]):
+                offspring1[i]=p2[i]
+                offspring2[i]=p1[i]
+            switch=1
+        else:
+            for i in range(xo_points[j-1],xo_points[j]):
+                offspring1[i]=p1[i]
+                offspring2[i]=p2[i]
+            switch=0
+
+    if switch==0:
+        for i in range(xo_points[-1],len(p1)):
+            offspring1[i]=p2[i]
+            offspring2[i]=p1[i]
+    else:
+        for i in range(xo_points[-1],len(p1)):
+            offspring1[i]=p1[i]
+            offspring2[i]=p2[i]
+
+    return offspring1, offspring2
+
 if __name__ == '__main__':
     p1, p2 = [9,8,4,5,6,7,1,3,2,10], [8,7,1,2,3,10,9,5,4,6]
+    p1,p2 = [1,2,3,4,5,6,7,8,9,10], [10,9,8,7,6,5,4,3,2,1]
+    p1,p2 = [1,0,0,0,1,1,0,1,1,0,1], [1,1,1,1,0,0,0,0,1,0,0]
     o1, o2 = arithmetic_xo(p1, p2)
+    o1, o2 = k_point_co(p1, p2,4)
+    print(o1, o2)
+    o1, o2 = k_point_co(p1, p2,1)
+    print(o1, o2)
+    o1, o2 = single_point_co(p1, p2)
+    print(o1, o2)
+    o1, o2 = pmx_co(p1, p2)
     print(o1, o2)
